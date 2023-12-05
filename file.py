1c4eac2be7e33a2c29eda22554199e070f06a513
Simon Dahal <simon.dahal@coderacademy.edu.au>
2023-12-05 21:05:16 +1100

    Add file.py and readme.txt

---
+++ file.py
@@ -0,0 +1,9 @@
+readme_file = open("readme.txt", "a")
+
+# print(readme_file.read())
+
+# readme_file.write("I am writing something in the file")
+
+readme_file.close()
+
+readme_file.write("salkdfhwe")
